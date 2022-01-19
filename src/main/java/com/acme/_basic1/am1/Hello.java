a8337f702de9adbf9460e7470c4157ac1e045403
rru <ankahikahani@gmail.com>
2022-01-19 17:38:42 +0100

    Initial commit

---
+++ src/main/java/com/acme/_basic1/am1/Hello.java
@@ -0,0 +1,13 @@
+package com.acme._basic1.am1;
+
+class Hello {
+
+  void sayHello() {
+    System.out.println("Hello World!!");
+  }
+
+  void notCovered() {
+    System.out.println("This method is not covered by unit tests.");
+  }
+
+}
---
+++ src/main/java/com/acme/basic1.simple/am1/Hello.java
@@ -0,0 +1,13 @@
+package com.acme.basic1.simple.am1;
+
+class Hello {
+
+  void sayHello() {
+    System.out.println("Hello World!!");
+  }
+
+  void notCovered() {
+    System.out.println("This method is not covered by unit tests.");
+  }
+
+}

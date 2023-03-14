f37ae5d09001719fba13d1782476b2d364e2939f
ankahikahani <95933675+ankahikahani@users.noreply.github.com>
2023-03-14 11:22:45 +0100

    Update Hi.java

--- src/main/java/com/acme/_basic1/Hi.java
+++ src/main/java/com/acme/_basic1/Hi.java
@@ -1,4 +1,8 @@
 package com.acme._basic1;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 class Hi {
 
@@ -9,5 +13,30 @@ class Hi {
   void notCovered() {
     System.out.println("This method is not covered by unit tests.");
   }
+  
+  
+	public static void getDataByFirstnameAndLastname(Connection con, String firstname, String lastname) {
+		Statement statement = null;
+		try {
+			statement = con.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		String queryString = "select * from test_table where first_name= '"+firstname +"'  And last_name= '"+lastname +"'";
+		ResultSet rs = null;
+		try {
+			rs = statement.executeQuery(queryString);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (rs != null) {
+				while (rs.next()) {
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }

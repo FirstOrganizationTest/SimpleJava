0baf7fec676451cad641b9b0a8defe78a76d8f06
ankahikahani <ankahikahani@gmail.com>
2024-10-01 16:29:20 +0200

    Update HelloWorld.java

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -12,6 +12,9 @@ public class HelloWorld {
      
     //new smell added to validate the second scenario
     System.out.println("This method is not covered by unit tests.");
+
+    //one more new smell added to validate the second scenario
+    System.out.println("This method is not covered by unit tests. Develop tests");
   }
 
 }

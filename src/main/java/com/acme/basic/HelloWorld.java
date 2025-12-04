7f0434a6833553286a80ed6b0433d0c104921a1a
ankahikahani <ankahikahani@gmail.com>
2025-12-04 16:40:24 +0100

    Update HelloWorld.java

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -2,11 +2,13 @@ package com.acme.basic;
 
 public class HelloWorld {
   int abc = 10;
+  
 
   void sayHello() {
     System.out.println("Hello World!!");
     int pqr = 20;
     int xyz = 30;
+    int ab = 25;
   }
 
   void notCovered() {

4f400268b6242a18684be2e98328eb436c32ff7e
ankahikahani <ankahikahani@gmail.com>
2025-12-05 08:56:31 +0100

    Remove unused variables from sayHello method
    Removed unused variable declarations from sayHello method.

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -6,14 +6,9 @@ public class HelloWorld {
 
   void sayHello() {
     System.out.println("Hello World!!");
-    int pqr = 20;
-    int xyz = 30;
-    int ab = 25;
-    int ssdd=40;
   }
 
   void notCovered() {
-     int xyz = 30;
     System.out.println("This method is not covered by unit tests.");
      System.out.println("This method is not covered by unit tests123456.");
   }
